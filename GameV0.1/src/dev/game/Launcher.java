2dccc991232449d5000de30f39a25b2e8ed945ad
Yukinon-arch <dolinskij.nikita@yandex.ru>
2019-12-05 13:42:56 +0200

    v.01

---
+++ GameV0.1/src/dev/game/Launcher.java
@@ -0,0 +1,14 @@
+package dev.game;
+import dev.display.Display;
+
+public class Launcher
+{
+
+	public static void main(String[] args)
+	{
+		Game game = new Game("Test",800,600);
+		game.start();
+
+	}
+
+}
